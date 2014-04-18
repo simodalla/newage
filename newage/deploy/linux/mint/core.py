9572617bf98292e1d3f22aadbf924173c18981e9
Simone Dalla <simodalla@gmail.com>
2014-04-18 14:49:33 +0200

    fix smbfs installl for mint13 and move config_network into deploy_root_teat_down for mint13

--- newage/deploy/linux/mint/core.py
+++ newage/deploy/linux/mint/core.py
@@ -32,18 +32,17 @@ class Mint(PamMountMixin, SicrawebMixin, MateMixin, RdesktopMixin,
         super(Mint, self).deploy_set_up()
 
     def deploy_run(self):
-        # self.config_network()
-        # self.prepare_ssh_autologin()
-        # with settings(warn_only=True):
-        #     self.update_apt_packages()
-        # self.prepare_python_env()
-        # self.prepare_rdesktop()
-        # self.prepare_sicraweb_jre(self.platform)
-        # self.prepare_ldap_client()
-        # self.prepare_home_skel()
-        # self.prepare_virtualenv_env()
-        # self.prepare_browsers(self.platform)
-        # self.prepare_mate_env()
+        self.prepare_ssh_autologin()
+        with settings(warn_only=True):
+            self.update_apt_packages()
+        self.prepare_python_env()
+        self.prepare_rdesktop()
+        self.prepare_sicraweb_jre(self.platform)
+        self.prepare_ldap_client()
+        self.prepare_home_skel()
+        self.prepare_virtualenv_env()
+        self.prepare_browsers(self.platform)
+        self.prepare_mate_env()
         self.prepare_pam_mount()
 
 
@@ -55,9 +54,11 @@ class Mint13(Mint):
               'rdesktop_1.7.1-1_amd64.deb'}
 
     def deploy_root_tear_down(self):
+        run('apt-get install -y smbfs')
         run('wget {}'.format(self.rdesktop_installers[self.platform]))
         run('dpkg -i {}'.format(
             self.rdesktop_installers[self.platform].split('/')[-1]))
+        self.config_network()
         super(Mint13, self).deploy_tear_down()
 
     def config_network(self, *args, **kwargs):
